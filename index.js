ee70479ed5042c917202513b01d43c5113723eaf
Lilisp16 <lijhosa@gmail.com>
2024-08-27 22:47:19 -0500

    Nueva configuración en la BD - archivo index.js

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ require('dotenv').config();
 //const urlMongoDb = "mongodb+srv://admin:0761163@cluster0.rkwsje2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 
-
 mongoose.connect(urlMongoDb, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -38,31 +37,6 @@ mongoose.connect(urlMongoDb, {
     console.error("Error de conexión:", error);
   }); */
 
-
-
-
-
-/*mongoose.connect(urlMongoDb,(err,res) => {
-    try {
-        if(err){
-            throw err;
-        }
-        else {
-            console.log("la conexion a la base de datos es correcta");
-
-            
-        app.listen(port, () => {
-        console.log("Server running at http://localhost:" + port);
-        })
-
-           
-        }
-
-    }catch (error) {
-        console.log(error);
-    }
-
-})*/
 
 
 
