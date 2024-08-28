a0882f50ee5f18970c0f63abe13e8472e7b1646b
Lilisp16 <lijhosa@gmail.com>
2024-08-27 21:25:56 -0500

    Cambios en la configuracion de la BD

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const port = 3000;
+const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const app = require('./app');
+
+// Usa la variable de entorno MONGODB_URI para conectar con MongoDB
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose.connect(mongoURI)
+  .then(() => console.log('Conexión a MongoDB exitosa'))
+  .catch(err => console.error('Error de conexión a MongoDB:', err));
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+
+
+/*const port = 3000;
 const app = require("./app");
 const mongoose = require("mongoose");
-const urlMongoDb = "mongodb+srv://admin:0761163@cluster0.rkwsje2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+require('dotenv').config();
+//const urlMongoDb = "mongodb+srv://admin:0761163@cluster0.rkwsje2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 
 
@@ -18,7 +36,7 @@ mongoose.connect(urlMongoDb, {
   })
   .catch((error) => {
     console.error("Error de conexión:", error);
-  });
+  }); */
 
 
 
